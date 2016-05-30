ccf91740d7ab93b650bfbba1cf658be24b24ea18
Remzi Kurshumliu <remzi04@gmail.com>
2016-05-30 20:09:35 +0200

    Update ReadMe.mk

--- ReadMe.mk
+++ ReadMe.mk
@@ -1,6 +1,6 @@
 # Getting and Cleaning Data
 
-## Course Project
+# Course Project
 
 You should create one R script called run_analysis.R that does the following.
 
@@ -12,10 +12,10 @@ You should create one R script called run_analysis.R that does the following.
 
 ## Steps to succcessfully complete this project
 
-1. Download the data source and put into a folder on your local drive. You'll have a ```UCI HAR Dataset``` folder.
-2. Put ```run_analysis.R``` in the parent folder of ```UCI HAR Dataset```, then set it as your working directory using ```setwd()``` function in RStudio.
-3. Run ```source("run_analysis.R")```, then it will generate a new file ```TinYData.txt``` in your working directory.
+1. Download the data source and put into a folder on your local drive. You'll have a "UCI HAR Dataset" folder.
+2. Put "run_analysis.R" in the parent folder of "UCI HAR Dataset", then set it as your working directory using "setwd()" function in RStudio.
+3. Run "source("run_analysis.R")", then it will generate a new file "TinYData.txt" in your working directory.
 
 ## Dependencies
 
-```run_analysis.R``` file will help you to install the dependencies automatically. It depends on ```reshape2``` and ```data.table```. 
+"run_analysis.R" file will help you to install the dependencies automatically. It depends on "reshape2" and "data.table". 
